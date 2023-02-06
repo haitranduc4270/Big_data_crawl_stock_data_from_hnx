1ddda9adefcc67c5f7dc18c1ed1773696bf7b589
racoon <hai.tranduc4270@gmail.com>
2023-02-07 00:12:54 +0700

    Check in exchange time

--- crawler/index.py
+++ crawler/index.py
@@ -2,10 +2,31 @@ from constant.constant import works, hadoop_namenode
 from services.crawl import start_crawl
 from dependencies import spark
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import config.config
 
 print('Craler stared')
+
+elasticsearch_time_format = '%Y-%m-%dT%H:%M:%S'
+
+
+def is_in_exchange_time():
+    now = datetime.now() + timedelta(hours=7)
+    today = now.strftime('%Y-%m-%d')
+
+    start_mor = datetime.strptime(
+        today + 'T' + '09:00:00', elasticsearch_time_format)
+    end_mor = datetime.strptime(
+        today + 'T' + '11:30:00', elasticsearch_time_format)
+    start_eve = datetime.strptime(
+        today + 'T' + '13:00:00', elasticsearch_time_format)
+    end_eve = datetime.strptime(
+        today + 'T' + '15:00:00', elasticsearch_time_format)
+
+    if (now >= start_mor and now <= end_mor) or (now >= start_eve and now <= end_eve):
+        return 1
+    else:
+        return 0
 
 
 def crawl(spark_sess, work):
@@ -24,10 +45,13 @@ def start_crawler():
     spark_sess = spark.start_spark()
 
     while True:
-        for work in works:
-            crawl(spark_sess, work)
+        if is_in_exchange_time() == 1:
+            for work in works:
+                crawl(spark_sess, work)
 
-        time.sleep(10)
+            time.sleep(10)
+        else:
+            print('Not exchange time yet')
 
 
 if __name__ == '__main__':
