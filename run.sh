29a1cb60f7f3527a72e91e39c3ec218cf26494e9
racoon <hai.tranduc4270@gmail.com>
2023-02-07 08:59:48 +0700

    Change container name

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 docker compose up -d
+
+elasticdump \
+  --input=http://localhost:9200/stock_data_realtime \
+  --output=31_1_mapping.json \
+  --type=mapping
+
+elasticdump \
+  --input=http://localhost:9200/stock_data_realtime \
+  --output=31_1_data.json \
+  --type=data
