9c263b6b2998a6ed8086c53c257776acbe988c84
Trần Đức Hải <hai.tranduc4270@gmail.com>
2024-08-14 16:43:18 +0700

    Update db.js

--- articles_server/config/db.js
+++ articles_server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDb() {
     try {
-        await mongoose.connect('mongodb+srv://hai4270:hai4270@cluster-big-data.m420aoy.mongodb.net/big-data?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://hai4270:hai4270@dev.uov9yto.mongodb.net/?retryWrites=true&w=majority');
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect fail!!!');
